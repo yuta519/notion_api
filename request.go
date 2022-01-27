fc929609e6ff096622e9451819d68a4e65125a48
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-27 09:51:50 +0900

    add new file request.go to treat http

---
+++ request.go
@@ -0,0 +1,46 @@
+package notionapi
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	notion_version string = "2021-08-16"
+	content_type   string = "application/json"
+)
+
+func Get(endpoint string, secret_token string) string {
+	req, _ := http.NewRequest("GET", endpoint, nil)
+	req.Header.Set("Notion-Version", notion_version)
+	req.Header.Set("Authorization", secret_token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	return string(body)
+}
+
+func Post(endpoint string, secret_token string, payload string) string {
+	req, _ := http.NewRequest("POST", endpoint, strings.NewReader(payload))
+	req.Header.Set("Notion-Version", notion_version)
+	req.Header.Set("Authorization", secret_token)
+	req.Header.Set("Content-Type", content_type)
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	return string(body)
+}
