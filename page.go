b9969a7611d40c5a92ba470189e4743ccf0fb282
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-28 09:05:05 +0900

    add new feature UpdatePage

--- page.go
+++ page.go
@@ -9,7 +9,7 @@ var (
 )
 
 func FetchPageList(secret_token string, db_id string) string {
-	response := http.PostRequest(
+	response := http.Post(
 		base_url+"databases/"+db_id+"/query",
 		secret_token,
 		"{\"page_size\":100}",
@@ -18,13 +18,29 @@ func FetchPageList(secret_token string, db_id string) string {
 }
 
 func CreatePage(secret_token string, db_id string, content string) string {
-	response := http.PostRequest(
+	response := http.Post(
 		base_url+"pages",
 		secret_token,
 		"{\"parent\": {\"database_id\": \""+db_id+"\"}, "+
 			// TODO: change `Name` to be able to input from argument
 			"\"properties\": {\"Name\": {\"title\": "+
 			"[{\"text\": {\"content\": \""+content+"\"}}]}}}",
+	)
+	return response
+}
+
+func UpdatePage(
+	secret_token string,
+	page_id string,
+	where string,
+	attribute string,
+	content string,
+) string {
+	response := http.Patch(
+		base_url+"pages/"+page_id,
+		secret_token,
+		"{\"properties\": { \""+where+"\": {\""+attribute+
+			"\": [{\"text\": {\"content\": \""+content+"\"}}]}}}",
 	)
 	return response
 }
