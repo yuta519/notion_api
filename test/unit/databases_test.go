57a5cba45d2a889b76fcb74696937e0ec98609ee
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-29 21:05:03 +0900

    change test status

--- test/unit/databases_test.go
+++ test/unit/databases_test.go
@@ -20,19 +20,10 @@ func TestFetchDatabaseIds(t *testing.T) {
 			[]byte("{\"object\":\"list\",\"results\":[],\"next_cursor\":null,\"has_more\":false}")),
 	)
 
-	res := notion_api.FetchDatabaseIds("")
-
-	// To test correct parsing
-	_, objectOk := res["object"]
-	_, resultsOk := res["results"]
-	_, nextcursorOk := res["next_cursor"]
-	_, hasMoreOk := res["has_more"]
-	if !objectOk || !resultsOk || !nextcursorOk || !hasMoreOk {
-		t.Errorf("Some fileds are parsed incorrectly.")
-	}
+	res := notion_api.FetchRawResponseOfDatabases("")
 
 	// To test a value of object key
-	if res["object"].(string) != "list" {
+	if res.Object != "list" {
 		t.Errorf("oh my godness")
 	}
 }
