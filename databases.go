f77c41d4cf020149fd1903f65a9ba68ba652b23e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-30 11:43:47 +0900

    define type at databases.go

--- databases.go
+++ databases.go
@@ -10,24 +10,46 @@ import (
 )
 
 type Databases struct {
-	HasMore    bool                     `json:"has_more"`
-	NextCursor interface{}              `json:"next_cursor"`
-	Object     string                   `json:"object"`
-	Results    []map[string]interface{} `json:"results"`
+	HasMore    bool        `json:"has_more"`
+	NextCursor interface{} `json:"next_cursor"`
+	Object     string      `json:"object"`
+	Results    []Database  `json:"results"`
 }
 
 type Database struct {
-	Cover          *Cover `json:"cover"`
-	CreatedTime    string `json:"created_time"`
-	Icon           *Icon  `json:"icon"`
-	Id             string `json:"id"`
-	LastEditedTime string `json:"last_edited_time"`
+	Cover          *Cover                 `json:"cover"`
+	CreatedTime    string                 `json:"created_time"`
+	Icon           *Icon                  `json:"icon"`
+	Id             string                 `json:"id"`
+	LastEditedTime string                 `json:"last_edited_time"`
+	Object         string                 `json:"object"`
+	Parent         Parent                 `json:"parent"`
+	Properties     map[string]interface{} `json:"properties"`
+	Title          []Title                `json:"title"`
+	Url            string                 `json:"url"`
 }
 
 type Cover struct {
+	External map[string]string `json:"external"`
+	Type     string            `json:"type"`
 }
 
 type Icon struct {
+	Emoji string `json:"emoji"`
+	Type  string `json:"type"`
+}
+
+type Parent struct {
+	Type      string `json:"type"`
+	Workspace bool   `json:"workspace"`
+}
+
+type Title struct {
+	Annotations map[string]interface{} `json:"annotations"`
+	Href        *string                `json:"href"`
+	PlainText   string                 `json:"plain_text"`
+	Text        map[string]string      `json:"text"`
+	Type        string                 `json:"type"`
 }
 
 func FetchRawResponseOfDatabases(secret_token string) Databases {
