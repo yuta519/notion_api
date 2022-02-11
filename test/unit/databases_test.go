31c2be3db07de877c7b6a4ed3842e28e861f9fe5
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-11 21:06:01 +0900

    create new page test

--- test/unit/databases_test.go
+++ test/unit/databases_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/yuta519/notion_api/utils"
 )
 
-func TestFetchDatabaseIds(t *testing.T) {
+func TestFetchDatabase(t *testing.T) {
 	// Mock Notion API response
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -24,6 +24,6 @@ func TestFetchDatabaseIds(t *testing.T) {
 
 	// To test a value of object key
 	if res.ObjectType != "list" {
-		t.Errorf("oh my godness")
+		t.Errorf("property `list` is missing")
 	}
 }
